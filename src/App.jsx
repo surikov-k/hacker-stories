49f51dcc4a904c6ac63fe5f46ee710929c0e560d
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-04 15:29:31 +0300

    Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
 
-const welcome = {
-  greeting: "Hey",
-  title: "React"
-};
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 
 function App() {
- 
+
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search"
+             type="text"/>
+      <hr/>
+
+      <ul>
+        {list.map((item) => {
+        return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
