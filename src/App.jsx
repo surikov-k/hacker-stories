45e7134693eb3a5547dc9dd5bc0c793038f3161a
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-04 15:30:15 +0300

    Components in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 const list = [
   {
     title: "React",
@@ -25,25 +23,40 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
+      <Search/>
+
+      <hr/>
+
+      <List/>
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      { list.map((item) => {
+        return (
+          <li key={ item.objectID }>
+              <span>
+                <a href={ item.url }>{ item.title }</a>
+              </span>
+            <span>{ item.author }</span>
+            <span>{ item.num_comments }</span>
+            <span>{ item.points }</span>
+          </li>
+        );
+      }) }
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search"
              type="text"/>
-      <hr/>
-
-      <ul>
-        {list.map((item) => {
-        return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 }
