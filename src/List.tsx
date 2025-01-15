81261264129f28cdbad45b333932c9713c79c71d
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-15 22:34:42 +0300

    Sorting

--- src/List.tsx
+++ src/List.tsx
@@ -1,16 +1,60 @@
+import { useState } from "react";
 import { Stories, Story } from "./App";
 import styles from "./App.module.css";
 import Check from "./assets/check.svg?react";
+import { sortBy } from "lodash";
 
 type ListProps = {
   list: Stories;
   onRemoveItem: (item: Story) => void;
 };
 
+const Sorts = {
+  NONE: (list: Stories) => list,
+  TITLE: (list: Stories) => sortBy(list, "title"),
+  AUTHOR: (list: Stories) => sortBy(list, "author"),
+  COMMENT: (list: Stories) => sortBy(list, "num_comments").reverse(),
+  POINT: (list: Stories) => sortBy(list, "points").reverse(),
+};
+
 export function List({ list, onRemoveItem }: ListProps): JSX.Element {
+  const [sort, setSort] = useState("NONE");
+
+  const handleSort = (sortKey: string) => {
+    setSort(sortKey);
+  };
+
+  const sortFunction = Sorts[sort];
+  const sortedList = sortFunction(list);
+
   return (
     <ul>
-      {list.map((item) => {
+      <li style={{ display: "flex" }}>
+        <span style={{ width: "40%" }}>
+          {" "}
+          <button type="button" onClick={() => handleSort("TITLE")}>
+            Title
+          </button>
+        </span>
+        <span style={{ width: "30%" }}>
+          <button type="button" onClick={() => handleSort("AUTHOR")}>
+            Author
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button type="button" onClick={() => handleSort("COMMENT")}>
+            Comments
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button type="button" onClick={() => handleSort("POINT")}>
+            Points
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>Actions</span>
+      </li>
+
+      {sortedList.map((item: Story) => {
         return (
           <Item key={item.objectID} onRemoveItem={onRemoveItem} item={item} />
         );
@@ -18,6 +62,7 @@ export function List({ list, onRemoveItem }: ListProps): JSX.Element {
     </ul>
   );
 }
+
 type ItemProps = {
   item: Story;
   onRemoveItem: (item: Story) => void;
